e0bd783f1e54f8ea4a4f24deb61ca78931932db8
dayuan.jiang <jdy.toh@gmail.com>
2023-02-15 00:49:51 +0900

    fix chatbot

--- chat_app.py
+++ chat_app.py
@@ -3,6 +3,8 @@ from streamlit_chat import message
 import requests
 from app import init_chain, get_docstore, format_result_to_markdown
 from langchain import OpenAI, PromptTemplate
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain.chains.question_answering import load_qa_chain
 
 
 from typing import Dict, List, Tuple
@@ -51,6 +53,28 @@ _template = """あなたはアクセンチュアのAIアシスタントとして
 独立した質問:"""
 CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
 
+def init_chain():
+    combine_prompt_template = """
+あなたは、アクセンチュアのAIアシスタントです。
+あなたには、以下のような長いドキュメントの抜粋部分とアクセンチュアに関する質問が与えられています。
+提供されたテキストを参考して答えてください。
+また、提供されたテキストに根拠がない場合、もしくは全部なしの場合は、わからないと答えなさい。答えを作り上げないでください。
+なお、質問には完全な文でお答えください。
+=========
+テキスト：{summaries}
+=========
+質問: {question}
+=========
+答案:"""
+    COMBINE_PROMPT = PromptTemplate(
+        template=combine_prompt_template, input_variables=["summaries", "question"]
+    )
+    chain = load_qa_with_sources_chain(OpenAI(temperature=0), 
+                                chain_type="stuff", 
+                                prompt=COMBINE_PROMPT,
+                                verbose=True,
+                                )
+    return chain
 
 if __name__ == "__main__":
     query = st.text_input("User Input")
@@ -58,6 +82,16 @@ if __name__ == "__main__":
     chain = init_chain()
     # llm = OpenAI(temperature=0, model_name="text-curie-001")
     llm = OpenAI(temperature=0)
+
+    template = """
+下記の質問をアクセンチェアに関する質問に書き換えてください。
+質問: {question}
+書き換えた質問:
+"""
+
+    query_temp = PromptTemplate(
+        template=template, input_variables=["question"]
+    )
     with st.spinner("考え中..."):
         if query:
             print("history: ", st.session_state.history)
@@ -68,11 +102,12 @@ if __name__ == "__main__":
                 )
                 rephrased_query = llm(formated_query_prompt)
             else:
-                rephrased_query = query
+                formated_query_prompt = query_temp.format(question= query)
+                rephrased_query = llm(formated_query_prompt)
                 
 
-            docs = vector_store.similarity_search(rephrased_query, k=10)
-            docs = [doc for doc in docs if len(doc.page_content) > 200][:3]
+            docs = vector_store.similarity_search(rephrased_query, k=2)
+            print(len(docs))
             result = chain(
                 {"input_documents": docs, "question": rephrased_query},
             )
